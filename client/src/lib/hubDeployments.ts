f9275e3e13a99ef21ceb63876a39283dd244870b
jaxernst <51183683+jaxernst@users.noreply.github.com>
2023-07-02 23:38:01 -0700

    Optimism goerli deployment

--- client/src/lib/hubDeployments.ts
+++ client/src/lib/hubDeployments.ts
@@ -1,7 +1,7 @@
-import { baseGoerli, hardhat } from "viem/chains";
+import { baseGoerli, hardhat, optimismGoerli } from "viem/chains";
 import type { EvmAddress } from "../types";
 
-export const deploymentChains = [baseGoerli, hardhat] as const;
+export const deploymentChains = [baseGoerli, hardhat, optimismGoerli] as const;
 export const deploymentChainIds = deploymentChains.map((c) => c.id);
 export const hubDeployments: Record<
   (typeof deploymentChainIds)[number],
@@ -9,4 +9,5 @@ export const hubDeployments: Record<
 > = {
   84531: "0x4a07b0728F9F52a64fD18C00C96a8a2bA5e34311",
   31337: "0x5fbdb2315678afecb367f032d93f642f64180aa3",
+  420: "0x4a07b0728F9F52a64fD18C00C96a8a2bA5e34311",
 };
