f629e7437ec07ba424dfd324f8e7c0b23087da25
jaxernst <51183683+jaxernst@users.noreply.github.com>
2023-07-06 23:20:17 -0700

    Add fun flip

--- client/src/alarms/AlarmsSidebar.svelte
+++ client/src/alarms/AlarmsSidebar.svelte
@@ -4,30 +4,40 @@
   import { get } from "svelte/store";
   import AlarmOverview from "./AlarmOverview.svelte";
   import { displayedAlarmId } from "./stores";
+  import { flip } from "svelte/animate"; // add this import
 
-  // Active alarms go first, then pending. Sort active alarms by alarm time (earliest -> latest)
-  $: sortedAlarms = Object.values($userAlarms)
-    .filter((alarm) => get(alarm).status !== AlarmStatus.CANCELLED)
+  // Split into active and non-active, sort active by time
+  $: activeAlarms = Object.values($userAlarms)
+    .filter((alarm) => get(alarm).status === AlarmStatus.ACTIVE)
     .sort((a, b) => {
-      if (get(a).status === AlarmStatus.ACTIVE) {
-        return -1;
-      }
-      return 1;
+      const aTime = get(a).timeToNextDeadline ?? 0;
+      const bTime = get(b).timeToNextDeadline ?? 0;
+      return Number(BigInt(aTime) - BigInt(bTime));
     });
 
+  $: nonActiveAlarms = Object.values($userAlarms).filter(
+    (alarm) =>
+      get(alarm).status !== AlarmStatus.ACTIVE &&
+      get(alarm).status !== AlarmStatus.CANCELLED
+  );
+
+  $: sortedAlarms = [...activeAlarms, ...nonActiveAlarms];
   $: alarmIds = sortedAlarms.map((alarm) => get(alarm).id);
+
   $: if ($displayedAlarmId && !alarmIds.includes($displayedAlarmId)) {
-    // Reset if the displayed alarm is no longer available
     $displayedAlarmId = undefined;
   }
 </script>
 
 <div class="max-h-[320px] overflow-y-auto">
   <div class="bg-transparent-grey flex flex-col rounded-xl">
-    {#each sortedAlarms as userAlarm}
-      {#if get(userAlarm).status !== AlarmStatus.CANCELLED}
-        <AlarmOverview {userAlarm} />
-      {/if}
+    {#each sortedAlarms as userAlarm (get(userAlarm).id)}
+      <div animate:flip={{ duration: 500 }}>
+        {#if get(userAlarm).status !== AlarmStatus.CANCELLED}
+          <!-- use the flip function to animate the list reordering -->
+          <AlarmOverview {userAlarm} />
+        {/if}
+      </div>
     {/each}
   </div>
 </div>
