a149fe4142d3bb8eabb04cbfe8f3090ae59183af
jaxernst <51183683+jaxernst@users.noreply.github.com>
2023-07-08 12:29:53 -0700

    Fix gas price setting

--- contracts/scripts/deploy.ts
+++ contracts/scripts/deploy.ts
@@ -1,13 +1,17 @@
 import { ethers } from "hardhat";
 import { SocialAlarmClockHub } from "../typechain-types";
 import { AlarmType, alarmTypeVals } from "../lib/types";
+import { parseUnits } from "ethers/lib/utils";
+
+let gasPrice = parseUnits("1", "gwei");
 
 /**
  * Deploy protocol hub and register commitment types
  */
 async function main() {
   const factory = await ethers.getContractFactory("SocialAlarmClockHub");
-  const CPH = await factory.deploy();
+  gasPrice = await ethers.provider.getGasPrice();
+  const CPH = await factory.deploy({ gasPrice });
   console.log("Deployed protocol hub to", CPH.address);
 
   await registerType(CPH, "PartnerAlarmClock");
@@ -16,8 +20,11 @@ async function main() {
 async function registerType(hub: SocialAlarmClockHub, type: AlarmType) {
   const contract = await (await ethers.getContractFactory(type)).deploy();
   await (
-    await hub.registerAlarmType(alarmTypeVals[type], contract.address)
+    await hub.registerAlarmType(alarmTypeVals[type], contract.address, {
+      gasPrice,
+    })
   ).wait();
+  console.log("Registered", type, "at", contract.address);
 }
 
 main().catch((error) => {
