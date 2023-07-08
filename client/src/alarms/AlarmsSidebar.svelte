88ae54b24624f69db5d983071de543baf2ce1761
jaxernst <51183683+jaxernst@users.noreply.github.com>
2023-07-08 10:41:36 -0700

    Fix sidebar reactivity

--- client/src/alarms/AlarmsSidebar.svelte
+++ client/src/alarms/AlarmsSidebar.svelte
@@ -1,32 +1,60 @@
 <script lang="ts">
   import { AlarmStatus } from "@sac/contracts/lib/types";
-  import { userAlarms } from "../lib/dappStores";
+  import { userAlarms, type UserAlarm } from "../lib/dappStores";
   import { get } from "svelte/store";
   import AlarmOverview from "./AlarmOverview.svelte";
   import { displayedAlarmId } from "./stores";
   import { flip } from "svelte/animate"; // add this import
+  import { onDestroy } from "svelte";
 
-  // Split into active and non-active, sort active by time
-  $: activeAlarms = Object.values($userAlarms)
-    .filter((alarm) => get(alarm).status === AlarmStatus.ACTIVE)
-    .sort((a, b) => {
-      const aTime = get(a).timeToNextDeadline ?? 0;
-      const bTime = get(b).timeToNextDeadline ?? 0;
-      return Number(BigInt(aTime) - BigInt(bTime));
+  // Keep track of the time to next deadline for each alarm (used for sorting)
+  let alarmTimesToNextDeadline: Record<string, bigint | undefined> = {};
+  let unsubs: (() => void)[] = [];
+  const unsubAllTimeToNextDeadlineListeners = () => {
+    unsubs.forEach((unsub) => unsub());
+    unsubs = [];
+  };
+
+  // manage time to next deadline listeners (Praying this doesn't cause a memory leak)
+  let activeAlarms: UserAlarm[] = [];
+  let nonActiveAlarms: UserAlarm[] = [];
+  userAlarms.subscribe(() => {
+    // Separate by status
+    activeAlarms = userAlarms.getByStatus([AlarmStatus.ACTIVE]);
+    nonActiveAlarms = userAlarms.getByStatus([
+      AlarmStatus.INACTIVE,
+      AlarmStatus.COMPLETE,
+      AlarmStatus.PAUSED,
+    ]);
+
+    // Reset listeners (lazy way to do this)
+    unsubAllTimeToNextDeadlineListeners();
+    activeAlarms.forEach((alarm) => {
+      unsubs.push(
+        alarm.subscribe((a) => {
+          alarmTimesToNextDeadline[a.id] = a.timeToNextDeadline;
+        })
+      );
     });
+  });
 
-  $: nonActiveAlarms = Object.values($userAlarms).filter(
-    (alarm) =>
-      get(alarm).status !== AlarmStatus.ACTIVE &&
-      get(alarm).status !== AlarmStatus.CANCELLED
-  );
+  $: sortedActiveAlarms = activeAlarms.sort((a, b) => {
+    const aTime = alarmTimesToNextDeadline[get(a).id];
+    const bTime = alarmTimesToNextDeadline[get(b).id];
+    if (aTime === undefined || bTime === undefined) {
+      return 0;
+    }
+    return aTime < bTime ? -1 : 1;
+  });
 
-  $: sortedAlarms = [...activeAlarms, ...nonActiveAlarms];
+  $: sortedAlarms = [...sortedActiveAlarms, ...nonActiveAlarms];
   $: alarmIds = sortedAlarms.map((alarm) => get(alarm).id);
 
   $: if ($displayedAlarmId && !alarmIds.includes($displayedAlarmId)) {
     $displayedAlarmId = undefined;
   }
+
+  onDestroy(unsubAllTimeToNextDeadlineListeners);
 </script>
 
 <div class="max-h-[320px] overflow-y-auto">
