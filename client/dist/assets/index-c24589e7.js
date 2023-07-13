93db98d5ddf07172187e6eaa3992c07ef5602f4a
jaxernst <51183683+jaxernst@users.noreply.github.com>
2023-07-12 18:50:14 -0600

    Revert "Rebuild"
    This reverts commit a3166fbd7fd65700b7666497c55f817ce589cf29.

--- client/dist/assets/index-c24589e7.js
+++ client/dist/assets/index-c24589e7.js
@@ -1,4 +1,4 @@
-import{A as Ke,i as g,n as d,b as x,t as E,s as w,x as n,o as P,a as U,e as p,c as Xe}from"./browser-99a01eee.js";import{X,Q as pe,N as y,G as I,i as m,Y as N,$ as Je,c as A,C as z}from"./index-e6f43646.js";import"./index-593d922b.js";/**
+import{A as Ke,i as g,n as d,b as x,t as E,s as w,x as n,o as P,a as U,e as p,c as Xe}from"./browser-99a01eee.js";import{X,Q as pe,N as y,G as I,i as m,Y as N,$ as Je,c as A,C as z}from"./index-d112c6c9.js";import"./index-6ec93536.js";/**
  * @license
  * Copyright 2018 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause
