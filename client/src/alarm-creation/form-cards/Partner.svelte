e9bbc3b5b32f867263a21161cc62c0047a6b9fde
jaxernst <51183683+jaxernst@users.noreply.github.com>
2023-07-18 18:16:41 -0600

    Support ens input

--- client/src/alarm-creation/form-cards/Partner.svelte
+++ client/src/alarm-creation/form-cards/Partner.svelte
@@ -1,12 +1,58 @@
-<script>
+<script lang="ts">
   import { isAddress } from "viem";
   import FormCard from "../FormCard.svelte";
-  import { otherPlayer } from "../alarmCreation";
+  import {
+    creationErrorMsg,
+    otherPlayer,
+    playerInputErrorMsg,
+  } from "../alarmCreation";
   import { getCurrentAccount } from "../../lib/chainClient";
+  import { fetchEnsAddress } from "@wagmi/core";
 
-  $: inputValid =
-    (isAddress($otherPlayer) || $otherPlayer.endsWith(".eth")) &&
-    $otherPlayer !== $getCurrentAccount().address;
+  let debouncedInput: string | undefined;
+  let inputValid: boolean;
+
+  $: checkInput = async (input: string) => {
+    let isEns =
+      input.length > 2 && (input.endsWith(".eth") || !input.startsWith("0x"));
+    let resolvedAddress: string | undefined = undefined;
+    if (isEns) {
+      try {
+        console.log("fetch ens");
+        const res = await fetchEnsAddress({
+          name: debouncedInput!,
+          chainId: 1,
+        });
+        if (res) resolvedAddress = res;
+      } catch (err) {
+        console.log("failed to fetch ens");
+        inputValid = false;
+        $playerInputErrorMsg = "Could not find ENS address";
+        return;
+      }
+    } else {
+      resolvedAddress = input;
+    }
+
+    if (resolvedAddress && !isAddress(resolvedAddress)) {
+      inputValid = false;
+      $playerInputErrorMsg = "Invalid address";
+    } else if (resolvedAddress === $getCurrentAccount().address) {
+      inputValid = false;
+      $playerInputErrorMsg = "Other player cannot be yourself";
+    } else {
+      inputValid = true;
+    }
+  };
+
+  let timer: NodeJS.Timeout;
+  $: otherPlayer.subscribe((val) => {
+    playerInputErrorMsg.set(undefined);
+    timer && clearTimeout(timer);
+    timer = setTimeout(() => {
+      checkInput(val);
+    }, 600);
+  });
 </script>
 
 <FormCard
