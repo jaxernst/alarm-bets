d7deae79ca5c318a218c2caf219b8fa91bf9ca99
jaxernst <51183683+jaxernst@users.noreply.github.com>
2023-08-17 23:13:45 -0700

    Last resort

--- client/dist/assets/index-20211fc8.js
+++ client/dist/assets/index-20211fc8.js
@@ -1,4 +1,4 @@
-import{A as Ge,i as g,n as d,b as x,t as E,s as w,x as i,o as P,a as U,e as p,c as Xe}from"./browser-99a01eee.js";import{n as X,s as pe,T as y,t as I,a as m,o as N,R as Je,p as A,y as z}from"./index-1062cebb.js";import"./index-b407402c.js";/**
+import{A as Ge,i as g,n as d,b as x,t as E,s as w,x as i,o as P,a as U,e as p,c as Xe}from"./browser-99a01eee.js";import{n as X,s as pe,T as y,t as I,a as m,o as N,R as Je,p as A,y as z}from"./index-42e5c376.js";import"./index-2d826b9a.js";/**
  * @license
  * Copyright 2018 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause
