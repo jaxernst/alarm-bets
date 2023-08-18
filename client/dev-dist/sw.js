dade4ad664ae9156e13d9468b33663a5dcfca0d4
jaxernst <51183683+jaxernst@users.noreply.github.com>
2023-08-17 23:25:30 -0700

    Agin

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-296dd616'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.6bhekr9jftg"
+    "revision": "0.mao3etq6d3g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
