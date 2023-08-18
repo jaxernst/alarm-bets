a8ed6e4e264dbfc2bbbaddf3f3c97189dd1b5ece
jaxernst <51183683+jaxernst@users.noreply.github.com>
2023-08-17 23:30:23 -0700

    Hopefully got it

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-296dd616'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.mao3etq6d3g"
+    "revision": "0.fdi8q1busq"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
