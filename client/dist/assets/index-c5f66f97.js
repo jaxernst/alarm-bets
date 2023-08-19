a9b7263541c92f71ff7a04d2fcad2ba6190c1345
jaxernst <51183683+jaxernst@users.noreply.github.com>
2023-08-18 17:48:23 -0700

    Fixed page in place

--- client/dist/assets/index-c5f66f97.js
+++ client/dist/assets/index-c5f66f97.js
@@ -1,4 +1,4 @@
-import{A as Ge,i as g,n as d,b as x,t as E,s as w,x as i,o as P,a as U,e as p,c as Xe}from"./browser-99a01eee.js";import{n as X,s as pe,T as y,t as I,a as m,o as N,R as Je,p as A,y as z}from"./index-d31afdb8.js";import"./index-9cc7bbcb.js";/**
+import{A as Ge,i as g,n as d,b as x,t as E,s as w,x as i,o as P,a as U,e as p,c as Xe}from"./browser-99a01eee.js";import{n as X,s as pe,T as y,t as I,a as m,o as N,R as Je,p as A,y as z}from"./index-e65257e4.js";import"./index-54a2fc1b.js";/**
  * @license
  * Copyright 2018 Google LLC
  * SPDX-License-Identifier: BSD-3-Clause
