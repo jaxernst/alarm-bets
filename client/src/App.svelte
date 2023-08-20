e23bf35ad6feaea708ce9a0e3a6e9d1cdf85e592
jaxernst <51183683+jaxernst@users.noreply.github.com>
2023-08-20 16:43:49 -0700

    Testy

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -39,6 +39,22 @@
   $: if (currentAlarms.length > 0) {
     $displayedAlarmId = get(currentAlarms[0]).id;
   }
+
+  let deferredPrompt;
+
+  onMount(() => {
+    window.addEventListener("beforeinstallprompt", (e) => {
+      console.log("Got");
+      // Prevent the mini-infobar from appearing on mobile
+      e.preventDefault();
+      // Stash the event so it can be triggered later.
+      deferredPrompt = e;
+      // Update UI notify the user they can install the PWA
+      // showInstallPromotion();
+      // Optionally, send analytics event that PWA install promo was shown.
+      console.log(`'beforeinstallprompt' event was fired.`);
+    });
+  });
 </script>
 
 <SvelteToast />
