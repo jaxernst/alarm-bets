1136828aef0f1941a148748d58079479b0f52338
jaxernst <51183683+jaxernst@users.noreply.github.com>
2023-09-04 22:48:06 -0700

    Mmove client to temp folder

--- client/src/types.ts
+++
@@ -1,1 +0,0 @@
-export type EvmAddress = `0x${string}`;
--- client/src/view.ts
+++
@@ -1,7 +0,0 @@
-import { writable } from "svelte/store";
-
-export type Tab = "alarms" | "new" | "join";
-export const activeTab = writable<Tab>("alarms");
-
-export const showWelcome = writable(true);
-export const welcomeHasBeenViewed = writable(false);
