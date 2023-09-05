048ed48fe54f0abc12d730dad47a9eedd1423eaa
jaxernst <51183683+jaxernst@users.noreply.github.com>
2023-09-04 22:49:25 -0700

    Type fix

--- client/src/lib/util.ts
+++ client/src/lib/util.ts
@@ -9,7 +9,7 @@ export function getPWADisplayMode() {
 	const isStandalone = window.matchMedia('(display-mode: standalone)').matches;
 	if (document.referrer.startsWith('android-app://')) {
 		return 'twa';
-	} else if (navigator.standalone || isStandalone) {
+	} else if ((navigator as any).standalone || isStandalone) {
 		return 'standalone';
 	}
 	return 'browser';
