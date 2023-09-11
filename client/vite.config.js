7baf0d39d34b7931c4b83f22a2001e47cd33968c
jaxernst <51183683+jaxernst@users.noreply.github.com>
2023-09-10 21:14:42 -0700

    PWA vite plugin

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,32 +1,9 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import { SvelteKitPWA } from '@vite-pwa/sveltekit';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [
-		sveltekit()
-		/*VitePWA({
-			registerType: 'autoUpdate',
-			manifest: {
-				name: 'Alarm Bets',
-				short_name: 'Alarm Bets',
-				icons: [
-					{
-						src: '/android-chrome-192x192.png',
-						sizes: '192x192',
-						type: 'image/png'
-					},
-					{
-						src: '/android-chrome-512x512.png',
-						sizes: '512x512',
-						type: 'image/png'
-					}
-				],
-				theme_color: '#252525',
-				background_color: '#252525',
-				display: 'standalone'
-			}
-		}) */
-	]
+	plugins: [sveltekit(), SvelteKitPWA()]
 };
 
 export default config;
