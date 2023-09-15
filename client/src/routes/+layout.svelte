05f814404ae286057e0c04e755f93b875c0b1c08
jaxernst <51183683+jaxernst@users.noreply.github.com>
2023-09-14 23:23:05 -0700

    Add auto update logic

--- client/src/routes/+layout.svelte
+++ client/src/routes/+layout.svelte
@@ -1,36 +1,49 @@
 <script>
 	import Welcome from '$lib/Welcome.svelte';
 	import { SvelteToast } from '@zerodevx/svelte-toast';
-	import { onMount } from 'svelte';
 	import { pwaInfo } from 'virtual:pwa-info';
 
-	onMount(async () => {
-		if (pwaInfo) {
-			const { registerSW } = await import('virtual:pwa-register');
-			registerSW({
-				immediate: true,
-				onRegistered(r) {
-					r &&
-						setInterval(() => {
-							console.log('Checking for sw update');
-							r.update();
-						}, 20000 /* 20s for testing purposes */);
-					console.log(`SW Registered: ${r}`);
-				},
-				onRegisterError(error) {
-					console.log('SW registration error', error);
-				}
-			});
-		}
-	});
-
 	$: webManifest = pwaInfo ? pwaInfo.webManifest.linkTag : '';
+
+	(function () {
+		// We are going to track an updated flag and an activated flag.
+		// When both of these are flagged true the service worker was updated and activated.
+		let updated = false;
+		let activated = false;
+		navigator.serviceWorker.register('service-worker.js').then((regitration) => {
+			regitration.addEventListener('updatefound', () => {
+				const worker = regitration.installing;
+				worker?.addEventListener('statechange', () => {
+					console.log({ state: worker.state });
+					if (worker.state === 'activated') {
+						// Here is when the activated state was triggered from the lifecycle of the service worker.
+						// This will trigger on the first install and any updates.
+						activated = true;
+						checkUpdate();
+					}
+				});
+			});
+		});
+		navigator.serviceWorker.addEventListener('controllerchange', () => {
+			// This will be triggered when the service worker is replaced with a new one.
+			// We do not just reload the page right away, we want to make sure we are fully activated using the checkUpdate function.
+			console.log({ state: 'updated' });
+			updated = true;
+			checkUpdate();
+		});
+
+		function checkUpdate() {
+			if (activated && updated) {
+				console.log('Application was updated refreshing the page...');
+				window.location.reload();
+			}
+		}
+	})();
 </script>
 
 <svelte:head>
 	{@html webManifest}
 </svelte:head>
-
 <SvelteToast />
 
 <Welcome />
