afe020872f04dd9f05711dec9b9f6ed2c0334165
jaxernst <51183683+jaxernst@users.noreply.github.com>
2023-09-16 13:45:58 -0700

    Remove duplicate import

--- client/src/routes/+layout.svelte
+++ client/src/routes/+layout.svelte
@@ -1,8 +1,6 @@
 <script>
 	import Welcome from '$lib/Welcome.svelte';
-	import { checkForServiceWorkerUpdate, subcribeToPushNotifications } from '$lib/util';
 	import { SvelteToast } from '@zerodevx/svelte-toast';
-	import { onMount } from 'svelte';
 	import { pwaInfo } from 'virtual:pwa-info';
 	import { onMount } from 'svelte';
 	import { checkForServiceWorkerUpdate } from '$lib/util';
