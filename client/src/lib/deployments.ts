934685a5be692da47ab6ae1ec3557fa5a7309276
jaxernst <51183683+jaxernst@users.noreply.github.com>
2023-09-27 21:17:21 -0700

    Fix deployment addresses

--- client/src/lib/deployments.ts
+++ client/src/lib/deployments.ts
@@ -7,11 +7,11 @@ export const deploymentChainIds = deploymentChains.map((c) => c.id);
 
 export const hubDeployments: Record<(typeof deploymentChainIds)[number], EvmAddress> = {
 	31337: '0x5fbdb2315678afecb367f032d93f642f64180aa3',
-	420: '0x345d6532B3542bE064407C31Eb8d7cBa4033Cd36'
+	420: '0x92eb8aD38ce67B3499CdEEaD96f97D45087c37D4'
 };
 
 export const partnerAlarmClockContractTemplates = {
-	420: '0x847cc5099d86e931216cDb8B3eCBC5acB354420e'
+	420: '0x345d6532B3542bE064407C31Eb8d7cBa4033Cd36'
 };
 
 export const deploymentBlockNumbers: Record<(typeof deploymentChainIds)[number], number> = {
