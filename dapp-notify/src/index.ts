df88fe04943715d44bd258f5a076b3d2e562c9b4
jaxernst <51183683+jaxernst@users.noreply.github.com>
2023-10-16 22:47:43 -0700

    Draft

--- dapp-notify/src/index.ts
+++ dapp-notify/src/index.ts
@@ -1,7 +1,10 @@
-import { createClient } from "@supabase/supabase-js";
+import { SupabaseClient, createClient } from "@supabase/supabase-js";
 import webpush from "web-push";
 import type { Database } from "../../alarm-bets-db";
 import { run } from "./user-alarm-notifications/scheduler";
+import { AlarmRow, EvmAddress } from "./types";
+import { AlarmStatus } from "@alarm-bets/contracts/lib/types";
+import { dbListener } from "./db-listener";
 
 require("dotenv").config({ path: "../.env" });
 
@@ -25,3 +28,96 @@ webpush.setVapidDetails(
 );
 
 run(supabaseClient);
+
+// Reactions
+// -> alarms change -> Cancel or start new nofitication schedules
+// -> notification change -> Cancel or start new notification schedules
+
+// Flow
+// 1. Get mapping of user notifications
+// 2. Build notification fetch function (built with current state of notifications)
+// 3. For each user, get their alarms
+// 4. For each alarm:
+//      - build function to get the next due date
+//      - build function to reschedule on delivery
+
+// 5. Set that schedule with the time to next deadline, when the notification fires,
+// the n
+
+// Return functions to get or fetch notification state
+function notificationsFetcher(supabase: SupabaseClient<Database>) {
+  // Update notifcation state on db changes
+  let notificationState = {};
+  dbListener(supabase, {
+    onNotificationRowAdded: () => {},
+    onNotificationRowDeleted: () => {},
+  });
+
+  const getOrFetchNotifications = (user: EvmAddress) => {
+    return;
+  };
+
+  return {
+    allNotifications: async () => {
+      const data = await supabaseClient.from("alarm_notifications").select("*");
+      notificationState = data;
+      return data;
+    },
+    userNotifications: getOrFetchNotifications,
+  };
+}
+
+function activeAlarmsFetcher(supabase: SupabaseClient<Database>) {
+  return async (user: EvmAddress) => {
+    const { data, error } = await supabaseClient
+      .from("partner_alarms")
+      .select("*")
+      .eq("status", AlarmStatus.ACTIVE)
+      .or(`player1.eq.${user}, player2.eq.${user}`);
+
+    if (error) {
+      throw new Error(
+        "Error fetching alarm notifications from Supabase: " + error.details
+      );
+    }
+
+    return data;
+  };
+}
+
+const globalTimers: Record<EvmAddress, Record<AlarmId, NodeJS.Timeout>> = {};
+
+const activeAlarms = activeAlarmsFetcher(supabaseClient);
+const { allNotifications, userNotifications } =
+  notificationsFetcher(supabaseClient);
+
+
+// Get user notification subscription state
+const users: Record<EvmAddress, Notifications> = (await allNotifications()).reduce((notif, acc) => {
+  return {
+    ...acc,
+    [user]
+  }
+}, [])
+
+// Make record of functions to get the schedule due date
+const alarms: Record<EvmAddress, () => Alarm[]>;
+const timeToNextDeadlineFetcher: Record<
+  EvmAddress,
+  Record<AlarmId, () => Promise<number>>
+>;
+
+const executors: Record<EvmAddress, Record<AlarmId, () => () => void>>;
+
+
+
+scheduleExecutor = (user: EvmAddress) => {
+  userNotifications(user)
+}
+
+
+const execute = scheduleExecutor();
+
+const key = "user-alarmId";
+
+const cancelTimer = execute(key, alarmDeadline());
--- dapp-notify/src/types.ts
+++ dapp-notify/src/types.ts
@@ -1,1 +1,5 @@
+import { Database } from "../../alarm-bets-db";
+
 export type EvmAddress = `0x${string}`;
+
+export type AlarmRow = Database["public"]["Tables"]["partner_alarms"]["Row"];
