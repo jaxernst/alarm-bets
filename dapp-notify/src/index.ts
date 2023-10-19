389d8d55d31254b5184d61f10a191d8265b1aca2
jaxernst <51183683+jaxernst@users.noreply.github.com>
2023-10-18 23:34:56 -0700

    FP wip

--- dapp-notify/src/index.ts
+++ dapp-notify/src/index.ts
@@ -1,10 +1,12 @@
 import { SupabaseClient, createClient } from "@supabase/supabase-js";
-import webpush from "web-push";
+import webpush, { sendNotification, PushSubscription } from "web-push";
 import type { Database } from "../../alarm-bets-db";
 import { run } from "./user-alarm-notifications/scheduler";
-import { AlarmRow, EvmAddress } from "./types";
+import { AlarmRow, EvmAddress, NotificationRow } from "./types";
 import { AlarmStatus } from "@alarm-bets/contracts/lib/types";
 import { dbListener } from "./db-listener";
+import { getTimeUntilNextAlarm } from "./util/time";
+import { parseAlarmDays } from "./util/util";
 
 require("dotenv").config({ path: "../.env" });
 
@@ -30,7 +32,7 @@ webpush.setVapidDetails(
 run(supabaseClient);
 
 // Reactions
-// -> alarms change -> Cancel or start new nofitication schedules
+// -> users alarm changes -> Cancel or start new nofitication schedules
 // -> notification change -> Cancel or start new notification schedules
 
 // Flow
@@ -44,8 +46,8 @@ run(supabaseClient);
 // 5. Set that schedule with the time to next deadline, when the notification fires,
 // the n
 
-// Return functions to get or fetch notification state
-function notificationsFetcher(supabase: SupabaseClient<Database>) {
+// Return functions to get or fetch notification state, with listeners to update the cached state
+/* function notificationsFetcher(supabase: SupabaseClient<Database>) {
   // Update notifcation state on db changes
   let notificationState = {};
   dbListener(supabase, {
@@ -53,23 +55,28 @@ function notificationsFetcher(supabase: SupabaseClient<Database>) {
     onNotificationRowDeleted: () => {},
   });
 
-  const getOrFetchNotifications = (user: EvmAddress) => {
-    return;
-  };
+  // Need some cache invalidation logic
+
+  const getOrFetchNotifications = (user: EvmAddress) => {};
 
   return {
     allNotifications: async () => {
-      const data = await supabaseClient.from("alarm_notifications").select("*");
-      notificationState = data;
+      const { data } = await supabase.from("alarm_notifications").select("*");
+
+      data?.reduce((record, row) => {
+        record[row.user_address as EvmAddress] = row;
+        return record;
+      }, {} as Record<EvmAddress, NotificationRow>);
+
       return data;
     },
     userNotifications: getOrFetchNotifications,
   };
-}
+} */
 
 function activeAlarmsFetcher(supabase: SupabaseClient<Database>) {
   return async (user: EvmAddress) => {
-    const { data, error } = await supabaseClient
+    const { data, error } = await supabase
       .from("partner_alarms")
       .select("*")
       .eq("status", AlarmStatus.ACTIVE)
@@ -85,39 +92,285 @@ function activeAlarmsFetcher(supabase: SupabaseClient<Database>) {
   };
 }
 
-const globalTimers: Record<EvmAddress, Record<AlarmId, NodeJS.Timeout>> = {};
+type AlarmId = number;
+type Alarm = {
+  alarmId: AlarmId;
+  alarmTime: number; // seconds after midnight in local timezone
+  alarmDays: number[]; // 1-7 (Sunday-Saturday)
+  timezoneOffset: number; // seconds from UTC
+};
 
-const activeAlarms = activeAlarmsFetcher(supabaseClient);
-const { allNotifications, userNotifications } =
-  notificationsFetcher(supabaseClient);
+const compactAlarmFormat = (a: AlarmRow): Alarm => ({
+  alarmId: a.alarm_id,
+  alarmTime: a.alarm_time,
+  alarmDays: parseAlarmDays(a.alarm_days),
+  timezoneOffset: a.timezone_offset,
+});
 
+const formattedFetchResult = <T extends any>(
+  fetcher: (user: EvmAddress) => Promise<AlarmRow[]>,
+  transformOutput: (a: AlarmRow) => T
+) => {
+  return async (user: EvmAddress) => {
+    const alarms = await fetcher(user);
+    return alarms.map(transformOutput);
+  };
+};
 
-// Get user notification subscription state
-const users: Record<EvmAddress, Notifications> = (await allNotifications()).reduce((notif, acc) => {
-  return {
-    ...acc,
-    [user]
-  }
-}, [])
+const sendPushNotification = async (subscription: PushSubscription) => {
+  await sendNotification(
+    subscription,
+    JSON.stringify({
+      title: "Upcoming Alarm",
+      body: "Your alarm submission window is open. Wake up and submit your alarm!",
+    })
+  );
+};
 
-// Make record of functions to get the schedule due date
-const alarms: Record<EvmAddress, () => Alarm[]>;
-const timeToNextDeadlineFetcher: Record<
-  EvmAddress,
-  Record<AlarmId, () => Promise<number>>
->;
+const pushNotification = (toDevices: () => Promise<NotificationRow[]>) => {
+  return async () => {
+    const devices = await toDevices();
+    devices.forEach((d) =>
+      sendPushNotification(d.subscription as unknown as PushSubscription)
+    );
+  };
+};
 
-const executors: Record<EvmAddress, Record<AlarmId, () => () => void>>;
+const timeToNextAlarm = ({ alarmTime, timezoneOffset, alarmDays }: Alarm) => {
+  return getTimeUntilNextAlarm(alarmTime, timezoneOffset, alarmDays);
+};
 
+type NotificationAddedEvent = {
+  name: "notificationAdded";
+  data: NotificationRow;
+};
 
+type NotificationDeletedEvent = {
+  name: "notificationDeleted";
+  data: NotificationRow;
+};
 
-scheduleExecutor = (user: EvmAddress) => {
-  userNotifications(user)
+type NotificationUpdatedEvent = {
+  name: "notificationUpdated";
+  data: NotificationRow;
+};
+
+type AlarmActivatedEvent = {
+  name: "alarmActivated";
+  data: AlarmRow;
+};
+
+type AlarmDeactivatedEvent = {
+  name: "alarmDeactivated";
+  data: AlarmRow;
+};
+
+type SchedulerEvent =
+  | NotificationAddedEvent
+  | NotificationDeletedEvent
+  | NotificationUpdatedEvent
+  | AlarmActivatedEvent
+  | AlarmDeactivatedEvent;
+
+function MakeDbEventSource(supabase: SupabaseClient<Database>) {
+  const queue: SchedulerEvent[] = [];
+
+  dbListener(supabase, {
+    onNotificationRowAdded: (row) =>
+      queue.push({ name: "notificationAdded", data: row }),
+    onNotificationRowDeleted: (row) =>
+      queue.push({ name: "notificationDeleted", data: row }),
+    onNotificationRowUpdated: (row) =>
+      queue.push({ name: "notificationUpdated", data: row }),
+    onAlarmActivated: (row) =>
+      queue.push({ name: "alarmActivated", data: row }),
+    onAlarmDeactivated: (row) =>
+      queue.push({ name: "alarmDeactivated", data: row }),
+  });
+
+  return () => queue.shift();
 }
 
+const getActiveAlarms = formattedFetchResult(
+  activeAlarmsFetcher(supabaseClient),
+  compactAlarmFormat
+);
 
-const execute = scheduleExecutor();
+type State = Record<
+  EvmAddress,
+  {
+    alarmTimers?: Record<
+      AlarmId,
+      {
+        timer: NodeJS.Timeout;
+        onDue: () => void;
+      }
+    >;
+    notifications?: NotificationRow[];
+  }
+>;
 
-const key = "user-alarmId";
+// Having problems figure out how to do this
+const scheduleAlarmEffect = (state: State, user: EvmAddress, alarm: Alarm) => {
+  return () => {
+    const timeout = timeToNextAlarm(alarm);
+    const timer = setTimeout(() => {
+      // Send push notification to devices
+      // Problem: State devices change change betweeen when the timer is set and fired
+    }, timeout);
 
-const cancelTimer = execute(key, alarmDeadline());
+    return {
+      ...state,
+      [user]: {
+        alarmTimers: {
+          [alarm.alarmId]: {
+            timer,
+            onDue: () => {
+              // Send push notification to devices
+              // Problem: State devices change change betweeen when the timer is set and fired
+            },
+          },
+        },
+      },
+    };
+  };
+};
+
+const scheduleAlarmsEffect = (
+  state: State,
+  user: EvmAddress,
+  alarms: Alarm[]
+): State => {
+  if (!alarms.length) return state;
+  const newState = scheduleAlarmEffect(state, user, alarms[0])();
+  return scheduleAlarmsEffect(newState, user, alarms.slice(1));
+};
+
+const scheduleActiveAlarmsEffect = (user: EvmAddress) => {
+  return async (state: State) => {
+    const alarms = await getActiveAlarms(user);
+    return scheduleAlarmsEffect(state, user, alarms);
+  };
+};
+
+type ScheduleActiveEffect = ["ScheduleActiveAlarms", { user: EvmAddress }];
+type RebuildAlarmTimersEffect = ["RebuildAlarmTimers", { user: EvmAddress }];
+type CancelAlarmTimersEffect = ["CancelAlarmTimers", { user: EvmAddress }];
+type Effect =
+  | ScheduleActiveEffect
+  | RebuildAlarmTimersEffect
+  | CancelAlarmTimersEffect;
+
+type ScheduleEventHandler<T extends any[]> = (
+  state: State,
+  ...args: T
+) => {
+  newState: State;
+  effects: Effect[];
+};
+
+const addNotificationDevice: ScheduleEventHandler<[NotificationRow]> = (
+  state: State,
+  notification: NotificationRow
+) => {
+  const user = notification.user_address as EvmAddress;
+  const notificationState = state[user] ?? {};
+  const newState = {
+    ...state,
+    [notification.user_address as EvmAddress]: {
+      ...notificationState,
+      notifications: [...(notificationState.notifications ?? []), notification],
+    },
+  };
+
+  const firstNotification = !notificationState.notifications;
+
+  return {
+    newState,
+    effects: [
+      firstNotification
+        ? ["ScheduleActiveAlarms", { user }]
+        : ["RebuildAlarmTimers", { user }],
+    ],
+  };
+};
+
+const removeNotificationDevice: ScheduleEventHandler<[NotificationRow]> = (
+  state: State,
+  notification: NotificationRow
+) => {
+  const user = notification.user_address as EvmAddress;
+  const notificationState = state[user] ?? {};
+  const newNotifications = notificationState.notifications?.filter(
+    (n) => n.device_id !== notification.device_id
+  );
+
+  const newState = {
+    ...state,
+    [user]: {
+      ...notificationState,
+      notifications: notificationState.notifications?.filter(
+        (n) => n.device_id !== notification.device_id
+      ),
+    },
+  };
+
+  const shouldCancelTimers = !newNotifications?.length;
+
+  return {
+    newState,
+    effects: [
+      shouldCancelTimers
+        ? ["CancelAlarmTimers", { user }]
+        : ["RebuildAlarmTimers", { user }],
+    ],
+  };
+};
+
+const handleSchedulerEvent: ScheduleEventHandler<[SchedulerEvent]> = (
+  state: State,
+  event: SchedulerEvent
+) => {
+  switch (event.name) {
+    case "notificationAdded":
+      return addNotificationDevice(state, event.data);
+    case "notificationDeleted":
+      return removeNotificationDevice(state, event.data);
+    case "notificationUpdated":
+      return updateNotificationDevice(event.data, state);
+    case "alarmActivated":
+      return scheduleAlarm(event.data, state);
+    case "alarmDeactivated":
+      return cancelAlarm(event.data, state);
+  }
+};
+
+async function processEffects(
+  state: State,
+  effects: ((state: State) => Promise<State> | State)[]
+) {
+  if (!effects.length) return state;
+  try {
+    const newState = await effects[0](state);
+    return processEffects(newState, effects.slice(1));
+  } catch (e) {
+    console.error(e);
+    return processEffects(state, effects.slice(1));
+  }
+}
+
+async function mainLoop(
+  getNextEvent: () => SchedulerEvent | undefined,
+  state: State
+) {
+  const event = getNextEvent();
+
+  if (!event) return mainLoop(getNextEvent, state);
+
+  const { newState, effects } = handleSchedulerEvent(state, event);
+  const finalState = await processEffects(newState, effects);
+
+  mainLoop(getNextEvent, finalState);
+}
+
+mainLoop(MakeDbEventSource(supabaseClient), {});
--- dapp-notify/src/types.ts
+++ dapp-notify/src/types.ts
@@ -3,3 +3,6 @@ import { Database } from "../../alarm-bets-db";
 export type EvmAddress = `0x${string}`;
 
 export type AlarmRow = Database["public"]["Tables"]["partner_alarms"]["Row"];
+
+export type NotificationRow =
+  Database["public"]["Tables"]["alarm_notifications"]["Row"];
